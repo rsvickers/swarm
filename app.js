b02ec36b6efe1e0ac0b8ba476a43f1e75b99e66a
Rhys Vickers <rs.vickers5@gmail.com>
2023-09-26 15:35:10 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,148 @@
+// SECTION variables
+
+const locations = [
+    '🏤', '🏥', '🏭', '🏢', '🏣'
+]
+
+const people = [{
+    name: 'Jimbo',
+    picture: '🤵',
+    location: '🏤',
+    killsVampires: false
+},
+{
+    name: 'Sammy',
+    picture: '🙆‍♀️',
+    location: '🏤',
+    killsVampires: false
+},
+{
+    name: 'Michael',
+    picture: '👷',
+    location: '🏤',
+    killsVampires: false
+},
+{
+    name: 'Robert',
+    picture: '👷',
+    location: '🏥',
+    killsVampires: false
+},
+{
+    name: 'Terry',
+    picture: '🤴',
+    location: '🏥',
+    killsVampires: false
+},
+{
+    name: 'Bill',
+    picture: '🕵️',
+    location: '🏥',
+    killsVampires: false
+},
+{
+    name: 'Marie',
+    picture: '👩‍🍳',
+    location: '🏭',
+    killsVampires: false
+},
+{
+    name: 'Mykeal',
+    picture: '💂',
+    location: '🏭',
+    killsVampires: false
+},
+{
+    name: 'Phil',
+    picture: '🧜‍♂️',
+    location: '🏭',
+    killsVampires: false
+},
+{
+    name: 'Wilson',
+    picture: '🏐',
+    location: '🏢',
+    killsVampires: false
+},
+{
+    name: 'Wendy',
+    picture: '👩‍⚕️',
+    location: '🏢',
+    killsVampires: false
+},
+{
+    name: 'Jeremy',
+    picture: '🦹',
+    location: '🏢',
+    killsVampires: false
+}
+]
+
+// SECTION functions
+
+function drawPeople() {
+    locations.forEach(personsLocation => {
+
+        const peopleAtLocation = people.filter(people => people.location == personsLocation)
+
+        // console.log('people at location', peopleAtLocation);
+
+        let pictureString = ''
+
+        peopleAtLocation.forEach(people => pictureString += people.picture)
+
+        const locationElement = document.getElementById(personsLocation)
+
+        locationElement.innerText = pictureString
+
+    })
+}
+
+
+function changeLocation() {
+    people.forEach(person => {
+        if (
+            person.picture == '🦇'
+        ) {
+            return
+        }
+
+        const randomLocationIndex = Math.floor(Math.random() * locations.length)
+        const randomLocation = locations[randomLocationIndex]
+        people.location = randomLocation
+
+    })
+
+    drawPeople()
+}
+
+
+function vampireHunter() {
+
+    const randomVampireHunterIndex = Math.floor(Math.random() * people.length)
+    const randomVampireHunter = people[randomVampireHunterIndex]
+
+    randomVampireHunter.killsVampires = true
+    // console.log('whose the vampire hunter:', people)
+
+}
+
+
+function attackPeople(selectedLocation) {
+    people.forEach(person => {
+        if (person.location == selectedLocation) {
+            person.picture = "🦇"
+            console.log(person)
+        }
+    })
+    drawPeople()
+}
+
+
+
+
+
+
+// SECTION calling functions
+vampireHunter()
+drawPeople() 
